bf0d2b1ef492e732660b43e0ebcd41be40ff3785
devtectiveFlex <devtective.exe@gmail.com>
2026-02-02 20:00:55 +0300

    chore: create supabase client initialization

---
+++ server/db/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseURL = process.env.SUPABASE_URL;
+const supabaseRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseRoleKey && !supabaseURL) {
+  throw new Error('Supabase env vars are not set');
+}
+
+export const supabase = createClient(supabaseURL!, supabaseRoleKey!)
